66246076dd848a644bf21b1e754c43701c0fdda6
esimo13 <obak139@gmail.com>
2023-06-17 19:02:06 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+const api_base = 'mongodb+srv://merntodo:merntodo13@cluster0.l6jnwhe.mongodb.net/?retryWrites=true&w=majority';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [todos, setTodos] = useState([]);
+	const [popupActive, setPopupActive] = useState(false);
+	const [newTodo, setNewTodo] = useState("");
+
+	useEffect(() => {
+		GetTodos();
+	}, []);
+
+	const GetTodos = () => {
+		fetch(api_base + '/todos')
+			.then(res => res.json())
+			.then(data => setTodos(data))
+			.catch((err) => console.error("Error: ", err));
+	}
+
+	const completeTodo = async id => {
+		const data = await fetch(api_base + '/todo/complete/' + id).then(res => res.json());
+
+		setTodos(todos => todos.map(todo => {
+			if (todo._id === data._id) {
+				todo.complete = data.complete;
+			}
+
+			return todo;
+		}));
+		
+	}
+
+	const addTodo = async () => {
+		const data = await fetch(api_base + "/todo/new", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json" 
+			},
+			body: JSON.stringify({
+				text: newTodo
+			})
+		}).then(res => res.json());
+
+		setTodos([...todos, data]);
+
+		setPopupActive(false);
+		setNewTodo("");
+	}
+
+	const deleteTodo = async id => {
+		const data = await fetch(api_base + '/todo/delete/' + id, { method: "DELETE" }).then(res => res.json());
+
+		setTodos(todos => todos.filter(todo => todo._id !== data._id));
+	}
+
+	return (
+		<div className="App">
+			<h1>Welcome</h1>
+			<h4>Your tasks</h4>
+
+			<div className="todos">
+				 {todos.map(todo=>(
+          <div key={todo._id}  className={`${todo.complete ? "todo is-complete" : "todo"}`}>
+
+          <div   className="checkbox"></div>
+          <div className="text">{todo.text}</div>
+          <div className="complete-todo" onClick={()=> completeTodo(todo._id)}>^</div>
+          <div className="delete-todo" onClick={()=> deleteTodo(todo._id)}>x</div>
+        </div>
+        ))}
+			</div>
+
+			<div className="addPopup" onClick={() => setPopupActive(true)}>+</div>
+
+			{popupActive ? (
+				<div className="popup">
+					<div className="closePopup" onClick={() => setPopupActive(false)}>X</div>
+					<div className="content">
+						<h3>Add Task</h3>
+						<input type="text" className="add-todo-input" onChange={e => setNewTodo(e.target.value)} value={newTodo} />
+						<div className="button" onClick={addTodo}>Create Task</div>
+					</div>
+				</div>
+			) : ''}
+		</div>
+	);
 }
 
 export default App;
